80b171ba77996680c17a23fd80a1b68091bd96de
Jaidev sah <jaidevsah007@gmail.com>
2022-10-07 10:10:22 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
 const App = () => {
+
+  const [getMarco, setMarco] = useState('Marco');
+  const [getPolo, setPolo] = useState('Polo');
+
+  const onChangeHandler=()=>{
+    if(getMarco == "Marco"){
+      setMarco("Polo");
+      setPolo("Marco");
+    }
+    else{
+      setMarco("Macro");
+      setPolo("Polo");
+    }
+  }
   
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{getMarco}</h1>
+      <button onClick={onChangeHandler} id="marco-polo-toggler">{getPolo}</button>
     </div>
   )
 }
